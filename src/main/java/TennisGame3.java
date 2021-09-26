21c0db0f71cfb8032718369adea8db88421de022
Camilo U <cauribeva@unal.edu.co>
2021-09-26 18:28:19 -0500

    Refactored TennisGame3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -2,9 +2,10 @@
 public class TennisGame3 implements TennisGame {
     
     private int pointsPlayer2;
-    private int pointsplayer1;
+    private int pointsPlayer1;
     private String player1Name;
     private String player2Name;
+
 
     public TennisGame3(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,24 +13,39 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (pointsplayer1 < 4 && pointsPlayer2 < 4 ) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[pointsplayer1];
-            return (pointsplayer1 == pointsPlayer2) ? s + "-All" : s + "-" + p[pointsPlayer2];
-        } else {
-            if (pointsplayer1 == pointsPlayer2)
-                return "Deuce";
-            s = pointsplayer1 > pointsPlayer2 ? player1Name : player2Name;
-            return ((pointsplayer1 - pointsPlayer2)*(pointsplayer1 - pointsPlayer2) == 1) ? "Advantage " + s : "Win for " + s;
-        }
+        int sumOfPoints = pointsPlayer1+pointsPlayer2;
+        int pointDifference = pointsPlayer1-pointsPlayer2;
+
+        if (isRegularDifference(sumOfPoints)) return caseRegularDifference(pointDifference);
+
+        if (pointDifference==0) return "Deuce";
+
+      return caseAdvantageOrWin(pointDifference);
+
     }
-    
+
+    private String caseRegularDifference(int pointDifference) {
+        String score;
+        String[] scoreDescriptions = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        score = scoreDescriptions[pointsPlayer1];
+        return (pointDifference == 0) ? score + "-All" : score + "-" + scoreDescriptions[pointsPlayer2];
+    }
+
+    private boolean isRegularDifference(int sumOfPoints) {
+        return sumOfPoints < 6 && (pointsPlayer1<4 && pointsPlayer2<4);
+    }
+
+    private String caseAdvantageOrWin(int pointDifference) {
+        String name = pointsPlayer1 > pointsPlayer2 ? player1Name : player2Name;
+        return (Math.abs(pointDifference)==1) ? "Advantage " + name : "Win for " + name;
+    }
+
+
     public void wonPoint(String playerName) {
-        if (playerName == player1Name)
-            this.pointsplayer1 += 1;
+        if (playerName.equals(player1Name))
+            pointsPlayer1++;
         else
-            this.pointsPlayer2 += 1;
+            pointsPlayer2++;
         
     }
 
